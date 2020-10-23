2894832558c76edc8a8fe793d113ac39979ba359
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-23 17:30:23 -0500

    add module.exports to the todo.router file. create GET route on server side

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -2,10 +2,26 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-// GET ROUTE
+// GET ROUTE - sends complete list of tasks from DB to client
+router.get('/', (req, res) => {
+  const queryText = 'SELECT * FROM "todo";';
+
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      console.log(dbResponse);
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 // POST ROUTE
 
 // UPDATE ROUTE
 
 // DELETE ROUTE
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ app.use(express.static('server/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// MODULARIZING ROUTES
+let todoRouter = require('./routes/todo.router');
+app.use('/todo', todoRouter);
+
 app.listen(PORT, () => {
   console.log('Server is running on port: ', PORT);
 });
