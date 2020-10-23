15dfa88c14bdf9756054c6fb9d06303e34d6fba0
Brad Loudis <Bradley.Loudis@gmail.com>
2020-10-23 16:10:39 -0500

    config pool in pool.js. require pool in todo.router.js and leave comments for where the routes will be added

---
+++ server/routes/todo.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+// GET ROUTE
+
+// POST ROUTE
+
+// UPDATE ROUTE
+
+// DELETE ROUTE
